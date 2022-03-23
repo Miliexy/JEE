8b19c6b7f5b1ee5f5e16cc492069ff10fffb5fdf
Ilyasse Lahrach <masterox@outlook.com>
2022-03-23 20:52:31 +0100

    Revert "Updating"
    This reverts commit 7f0a02945d7da9ee606dd8d6ac0be027248382ae.

--- Tp3_2/src/main/java/mili/ilyasse/tp3_2/repositories/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package mili.ilyasse.tp3_2.repositories;
-
-import mili.ilyasse.tp3_2.entities.patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<patient, Long> {
-}
