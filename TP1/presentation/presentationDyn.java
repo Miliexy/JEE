f6e7c5f82abf2692ac94beefaf57fd6abd39e33c
Ilyasse Lahrach <masterox@outlook.com>
2022-03-23 21:31:51 +0100

    Adding newf

--- TP1/presentation/presentationDyn.java
+++ TP1/presentation/presentationDyn.java
@@ -2,13 +2,17 @@ package tp1.presentation;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+<<<<<<< HEAD
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+=======
+>>>>>>> 2a91914 (New Commit)
 import java.util.Scanner;
 
 import tp1.dao.DaoImpl;
 import tp1.dao.IDao;
 import tp1.ext.DaoImplV2;
+<<<<<<< HEAD
 import tp1.metier.IMetier;
 import tp1.metier.MetierImpl;
 
@@ -32,6 +36,21 @@ public class presentationDyn {
         Method method = cMetier.getMethod("setDao",IDao.class);
         method.invoke(metier,dao);
         System.out.println("Result = " + metier.calcul());
+=======
+import tp1.metier.MetierImpl;
+
+public class presentationDyn {
+    
+
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+
+        Scanner scanner = new Scanner(new File("../config.txt"));
+        String daoClassName=scanner.nextLine();
+        Class cDao =Class.forName(daoClassName);
+        IDao dao = (IDao) cDao.newInstance();
+        System.out.println(daoClassName);
+>>>>>>> 2a91914 (New Commit)
     }
 }
 
